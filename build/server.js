c1722638086f0f4b7495f42703bfe7bc228b4de9
mohseenrm <mohseenmukaddam6@gmail.com>
2017-09-03 18:48:58 -0700

    updating server

--- build/server.js
+++ build/server.js
@@ -1,5 +1,23 @@
 'use strict';
 
-console.log('this is a sample');
+var argv = require('yargs').argv;
+var cors = require('cors');
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
 
-var x = 99;
+var app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/auth', function (request, response) {
+  console.log('Processing auth request....');
+  console.log('request: ', request.body);
+});
+
+app.set('port', process.env.PORT || argv.port || 8080);
+
+app.listen(app.get('port'), function () {
+  console.log('server listening on port : ' + app.get('port'));
+});
--- src/server.js
+++ src/server.js
@@ -1,3 +1,21 @@
-console.log('this is a sample');
+const argv = require('yargs').argv;
+const cors = require('cors');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
-const x = 99;
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/auth', (request, response) => {
+  console.log('Processing auth request....');
+  console.log('request: ', request.body);
+});
+
+app.set('port', process.env.PORT || argv.port || 8080);
+
+app.listen(app.get('port'), () => {
+  console.log(`server listening on port : ${app.get('port')}`);
+});
