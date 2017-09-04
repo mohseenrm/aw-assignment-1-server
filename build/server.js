c836ebaba02f1ebc995512f4fac8c3144221185a
mohseenrm <mohseenmukaddam6@gmail.com>
2017-09-04 10:55:42 -0700

    updating server with mongo connection

--- build/server.js
+++ build/server.js
@@ -4,14 +4,27 @@ var argv = require('yargs').argv;
 var cors = require('cors');
 var express = require('express');
 var bodyParser = require('body-parser');
+
+var mongo = require('mongodb').MongoClient;
 // const path = require('path');
+
+var db = null;
+
+var mostSecureUrl = 'mongodb://heroku_9ntks6wl:48d9flnlndmqsqdccqbfp2goko@ds123084.mlab.com:23084/heroku_9ntks6wl';
 
 var app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.post('/auth', function (request, response) {
+mongo.connect(mostSecureUrl, function (err, database) {
+  if (err) {
+    console.log('Error connecting to database!!');
+  }
+  db = database;
+});
+
+app.post('/auth/login', function (request, response) {
   console.log('Processing auth request....');
   console.log('request: ', request.body);
   response.json({
@@ -29,3 +42,15 @@ app.set('port', process.env.PORT || argv.port || 8080);
 app.listen(app.get('port'), function () {
   console.log('server listening on port : ' + app.get('port'));
 });
+
+var shutdown = function shutdown() {
+  console.log('Killing the server...');
+  if (db) {
+    console.log('Closing connection with database...');
+    db.close();
+  }
+};
+
+process.on('SIGTERM', shutdown);
+
+process.on('SIGINT', shutdown);
--- src/server.js
+++ src/server.js
@@ -2,12 +2,23 @@ const argv = require('yargs').argv;
 const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const mongo = require('mongodb').MongoClient;
 // const path = require('path');
+
+let db = null;
+
+const mostSecureUrl = 'mongodb://heroku_9ntks6wl:48d9flnlndmqsqdccqbfp2goko@ds123084.mlab.com:23084/heroku_9ntks6wl';
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+
+mongo.connect(mostSecureUrl, (err, database) => {
+  if (err) { console.log('Error connecting to database!!'); }
+  db = database;
+});
 
 app.post('/auth/login', (request, response) => {
   console.log('Processing auth request....');
@@ -27,3 +38,15 @@ app.set('port', process.env.PORT || argv.port || 8080);
 app.listen(app.get('port'), () => {
   console.log(`server listening on port : ${app.get('port')}`);
 });
+
+const shutdown = () => {
+  console.log('Killing the server...');
+  if (db) {
+    console.log('Closing connection with database...');
+    db.close();
+  }
+};
+
+process.on('SIGTERM', shutdown);
+
+process.on('SIGINT', shutdown);
